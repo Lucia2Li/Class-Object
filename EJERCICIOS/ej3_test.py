a455a5476cc1cb6ce44150a80fde650a2647ace6
Lucía Li <lucialiduarte@gmail.com>
2023-10-30 18:39:04 +0100

    Change 3

--- EJERCICIOS/ej3_test.py
+++ EJERCICIOS/ej3_test.py
@@ -1,8 +1,11 @@
 from ejercicio_3 import Rectangle
 from ejercicio_2 import Point
 
-p1 = Point(2,3)
-p2 = Point(4,5)
+p1 = Point(2,7)
+p2 = Point(5,8)
 
 r1 = Rectangle(p1,p2)
 
+r1.area()
+
+print(r1)
