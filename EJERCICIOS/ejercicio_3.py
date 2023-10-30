413620bf7bcf898379bb72fcf74c2a4f049077b4
Lucía Li <lucialiduarte@gmail.com>
2023-10-30 08:40:26 +0100

    Changes in 3

--- EJERCICIOS/ejercicio_3.py
+++ EJERCICIOS/ejercicio_3.py
@@ -1,2 +1,10 @@
+from ejercicio_2 import Point
 
+class Rectangle:
+    def __init__(self, p1, p2):
+        self.__p1 = p1
+        self.__p2 = p2
 
+    def __str__(self):
+        return f"({self.__p1}, {self.__p2})"
+    
