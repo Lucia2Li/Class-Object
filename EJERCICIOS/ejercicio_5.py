ee14ef965064228dd1d1e516776d0a863de83f8e
Lucía Li <lucialiduarte@gmail.com>
2023-11-02 10:45:27 +0100

    Change 5

--- EJERCICIOS/ejercicio_5.py
+++ EJERCICIOS/ejercicio_5.py
@@ -3,5 +3,12 @@ class Terminal:
     def __init__(self,phone_number):
         self.__phone_number = phone_number
     @property
-    def get_phone_number():
+    def get_phone_number(self):
         return self.__phone_number
+    
+    @numero.setter
+    def numero(self):
+       if self.__phone_number.startswith(9,6 or 7) and self.__phone_number <= 9:
+           return self.__phone_number
+       else:
+           raise ValueError("Invalid phone number please enter another one")
