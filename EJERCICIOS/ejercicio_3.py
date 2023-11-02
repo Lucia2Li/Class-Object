1fb6f5fab277a22d74e7991da92337139b2ff7c4
Lucía Li <lucialiduarte@gmail.com>
2023-11-02 20:13:03 +0100

    Change

--- EJERCICIOS/ejercicio_3.py
+++ EJERCICIOS/ejercicio_3.py
@@ -1,9 +1,11 @@
 from ejercicio_2 import Point
 
 class Rectangle:
-    def __init__(self, p1, p2):
+    def __init__(self, p1, p2,area, perimeter):
         self.__p1 = p1
         self.__p2 = p2
+        self.__area = 0
+        self.__perimeter = 0
 
     def __str__(self):
         return f"({self.__p1}, {self.__p2})"
@@ -13,4 +15,6 @@ class Rectangle:
     @property
     def perimeter(self,other_point):
         return 2 * ((self.__p1.x-other_point.x) + (self.__p1.y-other_point.y))
+    
+    
 
