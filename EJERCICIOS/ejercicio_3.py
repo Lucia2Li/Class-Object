766b3d46d9ec9705644657dfea10bede834d1ffd
Lucía Li <lucialiduarte@gmail.com>
2023-11-02 20:38:01 +0100

    chaneges in 3

--- EJERCICIOS/ejercicio_3.py
+++ EJERCICIOS/ejercicio_3.py
@@ -1,21 +1,21 @@
 from ejercicio_2 import Point
 
 class Rectangle:
-    def __init__(self, p1, p2,area, perimeter):
+    def __init__(self, p1, p2):
         self.__p1 = p1
         self.__p2 = p2
-        self.__area = 0
-        self.__perimeter = 0
+        self.area = 0
+        self.perimeter = 0
 
     def __str__(self):
         return f"({self.__p1}, {self.__p2})"
     
     @area.setter
-    def area(self,other_point):
-        return ((self.__p1.x-other_point.x)*(self.__p1.y-other_point.y))
+    def area(self):
+        return ((self.__p1.x-self.__p2.x)*(self.__p1.y-self.__p2.y))
     @perimeter.setter
     def perimeter(self,other_point):
-        return 2 * ((self.__p1.x-other_point.x) + (self.__p1.y-other_point.y))
+        return 2 * ((self.__p1.x-self.__2.x) + (self.__p1.y-self.__p2.y))
     @property
     def get_area(self):
         return self.__area
