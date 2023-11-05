f9efd41a3094d2f1962fb5e401ee3c62f0eb7aca
Lucía Li <lucialiduarte@gmail.com>
2023-11-05 22:14:35 +0100

    Changes

--- EJERCICIOS/ej3_test.py
+++ EJERCICIOS/ej3_test.py
@@ -1,11 +1,14 @@
 from ejercicio_3 import Rectangle
 from ejercicio_2 import Point
 
-p1 = Point(2,7)
-p2 = Point(4,5)
+p1 = Point(2, 8)
+p2 = Point(4, 5)
 
 r1 = Rectangle(p1,p2)
 
-print(r1)
+print(f"El rectángulo es: {r1}")
+print(f"El área del rectángulo es: {r1.area}")
+print(f"El perímetro del rectángulo es: {r1.perimeter}")
+
 
 
