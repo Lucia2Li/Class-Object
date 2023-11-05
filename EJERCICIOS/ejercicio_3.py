73fb43558ee06c8c245a881973112754b186a528
Lucía Li <lucialiduarte@gmail.com>
2023-11-05 22:10:24 +0100

    changes in 3

--- EJERCICIOS/ejercicio_3.py
+++ EJERCICIOS/ejercicio_3.py
@@ -4,31 +4,23 @@ class Rectangle:
     def __init__(self, p1, p2):
         self.__p1 = p1
         self.__p2 = p2
-        self.area = 0
-        self.perimeter = 0
+        self.__area = 0
+        self.__perimeter = 0
+
     @property
-    def get_area(self):
-        return self.__area
-    
-    @area.setter
     def area(self):
-        ((self.__p1.x-self.__p2.x)*(self.__p1.y-self.__p2.y))
+        self.__area = abs((self.__p1.x - self.__p2.x) * (self.__p1.y - self.__p2.y))
+        return self.__area
 
     @property
-    def get_perimeter(self):
-        return self.__perimeter
-
-    @perimeter.setter
     def perimeter(self):
-        2 * ((self.__p1.x-self.__2.x) + (self.__p1.y-self.__p2.y))
+        self.__perimeter = 2 * ((self.__p1.x - self.__p2.x) + (self.__p1.y - self.__p2.y))
+        return self.__perimeter
 
     def __str__(self):
         return f"({self.__p1}, {self.__p2})"
 
-    
 
-    
-    
     
     
 
