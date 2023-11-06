bd157353b25b8c97204b9f4b77e5b950093b51c3
Lucía Li <lucialiduarte@gmail.com>
2023-11-06 18:15:43 +0100

    Changes

--- EJERCICIOS/Terminal.py
+++ EJERCICIOS/Terminal.py
@@ -1,10 +1,19 @@
 #Implemtación de la clase Terminal
 class Terminal:
-    def __init__(self,phone_number):
-        self.__phone_number
+    def __init__(self,phone_number,digit):
+        self.__phone_number = phone_number
         
     @property
     def get_phone_number(self):
         return self.__phone_number
     
-    
+    @phone_number.setter
+    def phone_number(self):
+        lista = ["9","6","7"]
+        if phone_number[0] in lista:
+
+
+
+
+
+
