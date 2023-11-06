0cfd4864527fc87b9fa19f36811963a52ecc4b35
Lucía Li <lucialiduarte@gmail.com>
2023-11-06 17:36:28 +0100

    Change 5

--- EJERCICIOS/ejercicio_5.py
+++ EJERCICIOS/ejercicio_5.py
@@ -1,8 +1,13 @@
 #Implemtación de la clase Terminal
 class Terminal:
     def __init__(self,phone_number):
-        self.__phone_number = phone_number
+        lista = [9,8,7]
+        if self.__phone_number.startswith(lista):
+            self.__phone_number = phone_number
+        else:
+            raise ValueError("It should start with 9,6 or 7")
         
     @property
     def get_phone_number(self):
         return self.__phone_number
+    
