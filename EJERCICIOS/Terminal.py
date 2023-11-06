7568b9b41f85d129d7c3a3bbe9211a7eb81ed4c4
Lucía Li <lucialiduarte@gmail.com>
2023-11-06 18:27:38 +0100

    Change in Terminal

--- EJERCICIOS/Terminal.py
+++ EJERCICIOS/Terminal.py
@@ -1,16 +1,19 @@
 #Implemtación de la clase Terminal
 class Terminal:
     def __init__(self,phone_number,digit):
-        self.__phone_number = phone_number
+        self.phone_number = phone_number
         
     @property
     def get_phone_number(self):
         return self.__phone_number
     
     @phone_number.setter
-    def phone_number(self):
+    def phone_number(self, valor):
         lista = ["9","6","7"]
-        if phone_number[0] in lista:
+        if valor[0] in lista and len(valor)==9:
+            self.__phone_number = valor
+    
+        
 
 
 
