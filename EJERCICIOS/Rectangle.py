eeda6537f59bbb92ed6e3520e58c01282878ee73
Lucía Li <lucialiduarte@gmail.com>
2023-11-14 22:43:24 +0100

    Changes

--- EJERCICIOS/Rectangle.py
+++ EJERCICIOS/Rectangle.py
@@ -4,18 +4,15 @@ class Rectangle:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-        self.area = 0
-        self.perimeter = 0
 
     @property
     def area(self):
-        self.area = ((self.p1.x - self.p2.x) * (self.p1.y - self.p2.y))
-        return self.area
+        return abs((self.p1.x - self.p2.x) * (self.p1.y - self.p2.y))
+
 
     @property
     def perimeter(self):
-        self.perimeter = 2 * ((self.p1.x - self.p2.x) + (self.p1.y - self.p2.y))
-        return self.perimeter
+        return 2 * (abs(self.p1.x - self.p2.x) + abs(self.p1.y - self.p2.y))
 
     def __str__(self):
         return f"({self.p1}, {self.p2})"
