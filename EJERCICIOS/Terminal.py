20e3d9ab5f351effe4de9e83fe6fd879cc449e0a
Lucía Li <lucialiduarte@gmail.com>
2023-11-17 19:08:15 +0100

    Changes

--- EJERCICIOS/Terminal.py
+++ EJERCICIOS/Terminal.py
@@ -1,17 +1,28 @@
 #Implemtación de la clase Terminal
 class Terminal:
-    def __init__(self,phone_number,digit):
+    def __init__(self,phone_number):
         self.phone_number = phone_number
         
     @property
-    def get_phone_number(self):
+    def phone_number(self):
         return self.__phone_number
     
     @phone_number.setter
-    def phone_number(self, teléfono):
+    def phone_number(self, telefono):
         lista = ["9","6","7"]
-        if teléfono[0] in lista and len(teléfono) == 9 and teléfono.isdigit():
-            self.__phone_number = teléfono
+        if telefono[0] in lista and len(telefono) == 9 and telefono.isdigit():
+            self.__phone_number = telefono
+        else:
+            raise ValueError("El número es incorrecto")
+        
+    def __str__(self):
+        return f"El número de teléfono es {self.phone_number}"
+   
+t1 = Terminal("966112233")
+print(t1)
+
+
+
 
         
 
