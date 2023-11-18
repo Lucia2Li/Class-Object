a1061b7240a4e0a61f361eeaa86e6a4a2ed061bf
Lucía Li <lucialiduarte@gmail.com>
2023-11-18 11:20:03 +0100

    Change

--- EJERCICIOS/Terminal.py
+++ EJERCICIOS/Terminal.py
@@ -2,6 +2,9 @@
 class Terminal:
     def __init__(self,phone_number):
         self.phone_number = phone_number
+        self.tiempo = 0
+        self.tiempo_total = 0
+        self.tiempo_recivido = 0
         
         
     @property
@@ -15,16 +18,30 @@ class Terminal:
             self.__phone_number = telefono
         else:
             raise ValueError("El número es incorrecto")
-        
-   
-        
+    @property
+    def tiempo(self):
+        return self.__tiempo
+    @tiempo.setter
+    def tiempo(self,tiempo_llamada):
+        self.tiempo += tiempo_llamada
+        self.tiempo_total += tiempo_llamada
+    
+    @property
+    def tiempo_recibido(self):
+        return self.tiempo_recibido
+    
+    @tiempo_recibido.setter
+    def tiempo_recibido(self, tiempo_llamada):
+        self.tiempo_recibido = tiempo_llamada
+        self.tiempo_total = tiempo_llamada 
+
     def __str__(self):
         return f"El número de teléfono es {self.phone_number}"
    
 t1 = Terminal("966112233")
 t2 = Terminal("666744459")
 
-
+print(t1)
 
 
         
