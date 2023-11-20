5eb7487cb39c28e1d48354b0042aa279bb52e0d4
Lucía Li <lucialiduarte@gmail.com>
2023-11-20 18:39:25 +0100

    Changes

--- EJERCICIOS/Terminal.py
+++ EJERCICIOS/Terminal.py
@@ -3,7 +3,10 @@ class Terminal:
         self.phone_number = phone_number
         self.__time_call = 0
         self.__time_received = 0
-        self.__total_time = 0
+
+    def total_time(self):
+        return self.time_call+self.time_received    
+
     @property
     def phone_number(self):
         return self.__phone_number
@@ -20,34 +23,20 @@ class Terminal:
     @time_call.setter
     def time_call(self,time):
         self.__time_call += time
-        self.__total_time += time
     
     @property
     def time_received(self):
         return self.__time_received
     @time_received.setter
     def time_received(self,time):
-        self.__time_received = time
-        self.__total_time = time
+        self.__time_received += time
     
     def call(self, other_phone, time):
         self.time_call = time
         other_phone.time_received = time
     def __str__(self):
-        return f"{self.__phone_number}-Conversation time: {self.__total_time}"
-    
-t1 = Terminal("666112233")
-t2 = Terminal("666744459")
-t3 = Terminal("632128919")
-t4 = Terminal("664135818")
-print(t1)
-print(t2)
-t1.call(t2, 420)
-t1.call(t3, 210)
-print(t1)
-print(t2)
-print(t3)
-print(t4)
+        return f"{self.phone_number}-Conversation time: {self.total_time()}"
+
 
 
 class Mobile(Terminal):
@@ -78,6 +67,8 @@ class Mobile(Terminal):
         self.__charge = super().time_call * self.__rate
     
     def __str__(self):
-        return f"{super().phone_number}-{super().total_time}s of conversation - charged {self.charge}€"
-    
-       
+        return f"{super().phone_number}-{super().total_time()}s of conversation - charged {self.charge}€"
+
+
+
+
