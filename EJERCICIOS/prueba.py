0318368d6bdbcce4a8416611c2a9f1bf89bf3f58
Lucía Li <lucialiduarte@gmail.com>
2023-11-22 11:16:31 +0100

    Changes

--- EJERCICIOS/prueba.py
+++ EJERCICIOS/prueba.py
@@ -1,14 +1,18 @@
 lista_externa = [1, 2, 3, 4]
 lista_interna = ['a', 'b', 'c']
 lista_tercera = ["-","+","*"]
+lista_animal = ["perro","gato","loro"]
 
 for numero in lista_externa:
     
     for letra in lista_interna:
         
         for signo in lista_tercera:
-            
-            print(numero,letra,signo)
+
+            for animal in lista_animal:
+
+                print(numero,letra,signo,animal)
+
 
   
 
