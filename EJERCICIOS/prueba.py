d5e7172e39e97aaf22c316a1f8c9c7bbc7e65597
Lucía Li <lucialiduarte@gmail.com>
2023-11-23 14:35:46 +0100

    Changes

---
+++ EJERCICIOS/prueba.py
@@ -0,0 +1,9 @@
+lista_externa = [1,2,3,4]
+lista_interna1 = ["a","b","c"]
+lista_interna2 = ["EXO","MARVEL","ULA"]
+
+for numero in lista_externa:
+    for letra in lista_interna1:
+        for like in lista_interna2:
+            print(numero,letra,like)
+    
