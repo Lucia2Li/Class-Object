85090f416a2df80b5d4a04af27d07dfa394b3bbb
Lucía Li <lucialiduarte@gmail.com>
2023-11-23 16:26:56 +0100

    Changes

--- EJERCICIOS/prueba.py
+++ EJERCICIOS/prueba.py
@@ -1,6 +1,6 @@
 lista_externa = [1,2,3,4]
 lista_interna1 = ["a","b","c"]
-lista_interna2 = ["EXO","MARVEL","ULA"]
+lista_interna2 = ["EXO","MARs","ULA"]
 
 for numero in lista_externa:
     for letra in lista_interna1:
