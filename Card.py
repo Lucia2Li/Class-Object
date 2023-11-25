9994bc78b2755e01c88cad918b04c6fe5cb2380c
Lucía Li <lucialiduarte@gmail.com>
2023-11-25 09:59:00 +0100

    changes

--- Card.py
+++ Card.py
@@ -25,50 +25,48 @@ class Card:
         return f"{self.card}-{self.value}"
     
 #Class Hand
-    class Hand:
-        def __init__(self):
-            self.set_card = []  #vacio...
-        @property
-        def set_card(self):
-            return self.__set_card
-        @set_card.setter
-        def set_card(self,cards):
-            self.__set_cards = cards
+class Hand:
+    def __init__(self):
+        self.set_card = []  #vacio...
+    @property
+    def set_card(self):
+        return self.__set_card
+    @set_card.setter
+    def set_card(self,cards):
+        self.__set_cards = cards
 
 #Decartar una carta
-        def discard(self,card):
-            self.remove(card)
-        def draw(self, card):
-            self.set_card.append(card)
+    def discard(self,card):
+        self.remove(card)
+    def draw(self, card):
+        self.set_card.append(card)
 
-    class Deck:
-        def __init__(self):
-            self.deck = []
+class Deck:
+    def __init__(self):
+        self.deck = []
 
-        @property
-        def deck(self):
-            return self.__deck
-        @deck.setter
-        def deck(self,cards):
-            self.__deck = cards
+    @property
+    def deck(self):
+        return self.__deck
+    @deck.setter
+    def deck(self,cards):
+        self.__deck = cards
         
 
 #Método shuffle: baraja una lista  
-        def shuffle(self):
-            random.shuffle(self.deck)
+    def shuffle(self):
+        random.shuffle(self.deck)
 #Método draw: the first card can be taken from the deck
-        def draw(self):
-            self.deck.pop(0)
+    def hand_out(self):
+        self.deck.pop(0)
 
 #Deal a set of cards to a player, those cards are removed from the deck.
-        def deal(self,other_player):
-            return "Hola"
+    def deal(self,other_player):
+        return "Hola"
 #remove cards
-        def remove(self,card):
-            self.deck.remove(card)
-
-
-
+    def remove(self,card):
+        self.deck.remove(card)
 
 #hand.draw(deck.pop)/hand_out
 
+class Spanish_deck(Deck):
