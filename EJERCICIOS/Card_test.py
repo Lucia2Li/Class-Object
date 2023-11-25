3c2f05fa11482b749a4c1aae3e4ead99241d0e56
Lucía Li <lucialiduarte@gmail.com>
2023-11-25 17:40:26 +0100

    changes

--- EJERCICIOS/Card_test.py
+++ EJERCICIOS/Card_test.py
@@ -1,11 +1,18 @@
 from Card import Card, Hand, Deck, English_deck, Spanish_deck
 #Carta
 c1 = Card("Palos",6)
-print(c1)
+#print(c1)
 
 s = Spanish_deck()
-print(s)
-print(s.hand_out())
-print(s.hand_out())
-print(s.shuffle())
-print(s)
+#print(s)
+#print(s.hand_out())
+#print(s.hand_out())
+#print(s.shuffle())
+#print(s)
+
+
+hand = Hand()
+e = English_deck()
+e.deal(hand, 5)
+print(e.hand_out())
+print(hand)
